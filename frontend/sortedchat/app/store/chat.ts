61cf0ce945dbbab6e5b284bfbef3c2723af45574
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2025-05-16 00:10:48 +0530

    stream init

--- frontend/sortedchat/app/store/chat.ts
+++ frontend/sortedchat/app/store/chat.ts
@@ -1,15 +1,13 @@
+import { HelloRequest } from "proto/chatservice_pb";
 import { ChatRequest, SortedChatClient } from "../../proto/chatservice"
 
 var chat = new SortedChatClient("http://localhost:8080")
 
-async function doChat(msg: string) {
-    const response = await chat.Chat(
-        ChatRequest.fromObject({
-            text: msg,
-    }),
-    {}
-    )
-    return response.text
+async function doChat(msg: string) {    
+  const request = new HelloRequest();
+  request.setText(msg);
+
+  const stream = chat.lotsOfReplies(request, {});
 }
 
 export { doChat }
--- frontend/sortedchat/proto/chatservice_pb.d.ts
+++ frontend/sortedchat/proto/chatservice_pb.d.ts
@@ -43,3 +43,43 @@ export namespace ChatResponse {
   }
 }
 
+export class HelloRequest extends jspb.Message {
+  getText(): string;
+  setText(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): HelloRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: HelloRequest): HelloRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: HelloRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): HelloRequest;
+  static deserializeBinaryFromReader(message: HelloRequest, reader: jspb.BinaryReader): HelloRequest;
+}
+
+export namespace HelloRequest {
+  export type AsObject = {
+    text: string,
+  }
+}
+
+export class HelloResponse extends jspb.Message {
+  getText(): string;
+  setText(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): HelloResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: HelloResponse): HelloResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: HelloResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): HelloResponse;
+  static deserializeBinaryFromReader(message: HelloResponse, reader: jspb.BinaryReader): HelloResponse;
+}
+
+export namespace HelloResponse {
+  export type AsObject = {
+    text: string,
+  }
+}
+

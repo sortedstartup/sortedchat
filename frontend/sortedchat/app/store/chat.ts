751c35527dde6f0fd7b56f958168cf6dca5f76e9
xask00 <xask.linus@gmail.com>
2025-05-16 16:54:49 +0000

    fix for app to work on codespaces

--- frontend/sortedchat/app/store/chat.ts
+++ frontend/sortedchat/app/store/chat.ts
@@ -1,7 +1,7 @@
 import { HelloRequest } from "proto/chatservice_pb";
 import { ChatRequest, SortedChatClient } from "../../proto/chatservice"
 
-var chat = new SortedChatClient("http://localhost:8080")
+var chat = new SortedChatClient(import.meta.env.VITE_API_URL)
 
 async function doChat(msg: string) {    
   const request = new HelloRequest();
--- frontend/sortedchat/vite.config.ts
+++ frontend/sortedchat/vite.config.ts
@@ -3,6 +3,18 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+
+  server: {
+    proxy: command === 'serve' ? {
+      '/hack': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/hack/, '')
+      }
+    } : undefined
+  }
+
+}));

e719b50b87b3f5492ae382b88b51a17af4c22920
sanskaraggarwal2025 <77196160+sanskaraggarwal2025@users.noreply.github.com>
2025-05-16 17:07:33 +0000

    dochat

--- frontend/sortedchat/app/store/chat.ts
+++ frontend/sortedchat/app/store/chat.ts
@@ -1,13 +1,28 @@
-import { HelloRequest } from "proto/chatservice_pb";
-import { ChatRequest, SortedChatClient } from "../../proto/chatservice"
+import { ChatRequest, HelloRequest, HelloResponse, SortedChatClient } from "../../proto/chatservice"
 
 var chat = new SortedChatClient(import.meta.env.VITE_API_URL)
 
-async function doChat(msg: string) {    
-  const request = new HelloRequest();
-  request.setText(msg);
+function doChat(
+  message: string,
+  onMessage: (chunk: string) => void,
+  onComplete?: () => void,
+  onError?: (err: any) => void
+) {
+  const req = HelloRequest.fromObject({ text: message });
 
-  const stream = chat.lotsOfReplies(request, {});
+  const stream = chat.LotsOfReplies(req, {});
+
+  stream.on("data", (res: HelloResponse) => {
+    onMessage(res.text);
+  });
+
+  stream.on("end", () => {
+    onComplete?.();
+  });
+
+  stream.on("error", (err) => {
+    onError?.(err);
+  });
 }
 
 export { doChat }

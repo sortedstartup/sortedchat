4b3881fcca8bc1751397e8fe29273239e78b0fb5
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2025-06-12 00:04:14 +0530

    remove upload page

--- frontend/sortedchat/app/routes.ts
+++ frontend/sortedchat/app/routes.ts
@@ -3,5 +3,4 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("chat/:id", "routes/chat.$id.tsx"),
-  route("upload", "routes/upload.tsx")
 ] satisfies RouteConfig;

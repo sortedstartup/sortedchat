81cd21f7ca6a26c6544f20cc6e9d29f303547a52
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2025-07-29 14:18:09 +0530

    chat component resize

--- frontend-new/src/App.tsx
+++ frontend-new/src/App.tsx
@@ -2,13 +2,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Chat } from "./pages/chat";
 import { Project } from "./pages/project";
 import { Home } from "./pages/home";
+import { Layout } from "./layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/chat/:chatId", element: <Chat /> },
-  // { path: "/login", element: <Login /> },
-  { path: "/project/:projectId", element: <Project /> },
-  // { path: "/setting", element: <Setting /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true, 
+        element: <Home />,
+      },
+      {
+        path: "chat/:chatId",
+        element: <Chat />,
+      },
+      {
+        path: "project/:projectId",
+        element: <Project />,
+      },
+    ],
+  },
 ]);
 
 function App() {
--- frontend-new/src/main.tsx
+++ frontend-new/src/main.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
-import "./index.css"
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarTrigger />
     <App />
-    </SidebarProvider>
   </React.StrictMode>
 );

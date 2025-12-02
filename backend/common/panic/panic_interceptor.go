83749dd6c1ac0e7b9c59317c7eb7ea4a5e09ffa8
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2025-12-02 18:03:32 +0530

    added panic interceptor

---
+++ backend/common/panic/panic_interceptor.go
@@ -0,0 +1,46 @@
+package panic
+
+import (
+	"context"
+	"log/slog"
+	"runtime"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func UnaryInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		defer func() {
+			if p := recover(); p != nil {
+				panicRecoveryHandler(p, info.FullMethod)
+
+				err = status.Errorf(codes.Internal, "internal server error")
+			}
+		}()
+		return handler(ctx, req)
+	}
+}
+
+func StreamInterceptor() grpc.StreamServerInterceptor {
+	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) (err error) {
+		defer func() {
+			if p := recover(); p != nil {
+				panicRecoveryHandler(p, info.FullMethod)
+				err = status.Errorf(codes.Internal, "internal server error")
+			}
+		}()
+		return handler(srv, ss)
+	}
+}
+
+func panicRecoveryHandler(p interface{}, info string) error {
+	stack := make([]byte, 4096)
+	length := runtime.Stack(stack, false)
+	slog.Error("panic recovered in stream RPC",
+		"method", info,
+		"panic", p,
+		"stack", string(stack[:length]))
+	return status.Errorf(codes.Internal, "internal server error")
+}
